b6f509ceaf3f0f8341986dcf072d0ced8fa94df8
Lynette7 <lynettewanjiru7@gmail.com>
2023-10-05 18:15:36 +0300

    chore: Starknet basecamp frontend

---
+++ basecamp-frontend/src/App.js
@@ -0,0 +1,124 @@
+import './App.css'
+import { connect, disconnect } from '@argent/get-starknet'
+import { useState, useEffect } from 'react'
+import { Contract, Provider, constants } from 'starknet'
+
+import contractAbi from './abis/abi.json'
+const contractAddress = "0x077e0925380d1529772ee99caefa8cd7a7017a823ec3db7c003e56ad2e85e300"
+
+function App() {
+  const [connection, setConnection] = useState('');
+  const [account, setAccount] = useState('');
+  const [address, setAddress] = useState('');
+
+  const [retrievedValue, setRetrievedValue] = useState('')
+
+  useEffect(() => {
+    const connectToStarknet = async() => {
+      const connection = await connect({ modalMode: "neverAsk", webWalletUrl: "https://web.argent.xyz" })
+
+      if (connection && connection.isConnected) {
+        setConnection(connection)
+        setAccount(connection.account)
+        setAddress(connection.selectedAddress)
+      }
+    }
+    connectToStarknet()
+  }, [])
+  
+  const connectWallet = async() => {
+    const connection = await connect({ webWalletUrl: "https://web.argent.xyz" })
+
+    if (connection && connection.isConnected) {
+      setConnection(connection)
+      setAccount(connection.account)
+      setAddress(connection.selectedAddress)
+    }
+  }
+
+  const disconnectWallet = async() => {
+    await disconnect()
+    setConnection(undefined)
+    setAccount(undefined)
+    setAddress('')
+  }
+
+  const increaseCounter = async() => {
+    try{
+      const contract = new Contract(contractAbi, contractAddress, account)
+      await contract.increment()
+      alert("You successfully increased the counter")
+    }
+    catch(error) {
+      console.log(error.message)
+    }
+  }
+
+  const decreaseCounter = async() => {
+    try {
+      const contract = new Contract(contractAbi, contractAddress, account)
+      await contract.decrement()
+      alert("You successfully decreased the counter")
+    }
+    catch(error) {
+      console.log(error.message)
+    }
+  }
+
+  const getCounter = async() => {
+    const provider = new Provider( {sequencer: { network:constants.NetworkName.SN_MAIN}})
+    try {
+      const contract = new Contract(contractAbi, contractAddress, provider)
+      const counter = await contract.get_current_count()
+      setRetrievedValue(counter.toString())
+    }
+    catch(error) {
+      console.log(error.message)
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <main className="main">
+          <h1 className="title">
+            Starknet<a href="#"> Counter</a>
+          </h1>
+            {
+              connection ?
+              <button className="connect" onClick={disconnectWallet}>Disonnect wallet</button>
+              :
+              <button className="connect" onClick={connectWallet}>Connect wallet</button>
+            }
+            <button className="connect" onClick={connectWallet}>Connect wallet</button>
+
+          <p className="description">
+          {
+            address ? `Connected to ${address}` : 'Not connected'
+          }
+          </p>
+
+          <div className="grid">
+            <div href="#" className="card">
+              <h2>Ensure to connect to Mainnet! &rarr;</h2>
+
+              <p>Increase/Decrease Counter.</p>
+              <div className="cardForm">
+                <input type="submit" className="button" value="Increase" onClick={increaseCounter} />
+                <input type="submit" className="button" value="Decrease" onClick={decreaseCounter} />
+              </div>
+
+              <hr />
+              <div className="cardForm">
+                <input type="submit" className="button" value="Get Counter" onClick={getCounter} />
+                <p>{retrievedValue}</p>
+              </div>
+            </div>
+          </div>
+        </main>
+      </header>
+    </div>
+  );
+}
+
+export default App;
